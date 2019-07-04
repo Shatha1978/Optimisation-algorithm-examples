586ca92cdf549a7603bab0900a271611b6de047e
Your Name <you@example.com>
2019-07-04 13:39:06 +0100

    Commented the class.

--- src/RouletteWheel.py
+++ src/RouletteWheel.py
@@ -1,9 +1,14 @@
+# Import the random package to radomly select individuals
 import random
 
+# Import the superclass (also called base class), which is an abstract class,
+# to implement the subclass ThresholdSelection
 from SelectionOperator import *
 
+# The subclass that inherits of SelectionOperator
 class RouletteWheel(SelectionOperator):
 
+    # Constructor
     def __init__(self):
         super().__init__("Roulette wheel selection");
         self.sum_fitness = 0.0
@@ -11,15 +16,19 @@ class RouletteWheel(SelectionOperator):
         # Get a SystemRandom instance out of random package
         self.system_random = random.SystemRandom();
 
+    # Sum the fitness of all the individuals (run once per generation before any selection is done)
+    # anIndividualSet: The set of individual to choose from
     def preProcess(self, anIndividualSet):
         # Compute fitness sumation
         self.sum_fitness = 0.0
         for individual in anIndividualSet:
             self.sum_fitness += individual.fitness
 
-    def __select__(self, anIndividualSet, aFlag): # aFlag == True for selecting good individuals,
-                                                  # aFlag == False for selecting bad individuals,
-
+    # Select an idividual
+    # anIndividualSet: The set of individual to choose from
+    # aFlag == True for selecting good individuals,
+    # aFlag == False for selecting bad individuals,
+    def __select__(self, anIndividualSet, aFlag):
 
         if aFlag == False:
             raise NotImplementedError("Selecting a bad individual is not implemented in RouletteWheel!")
