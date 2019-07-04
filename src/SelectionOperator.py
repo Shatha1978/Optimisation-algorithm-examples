889bfbba07e2585e70f52548fa7cd9c2adb53b62
Your Name <you@example.com>
2019-07-04 13:34:13 +0100

    Commented the class.

--- src/SelectionOperator.py
+++ src/SelectionOperator.py
@@ -1,27 +1,38 @@
+# The superclass to implement selection operators.
+# It is an abstract class.
 class SelectionOperator:
 
+    # Constructor
     def __init__(self, name = "Unspecified selection operator"):
         self.name = name;
 
-
-    def getName(self):
+    # Accessor on the name of the operator
+    def getName(self) -> str:
         return self.name;
 
+    # Select a good individual from anIndividualSet
     def select(self, anIndividualSet):
         return self.selectGood(anIndividualSet);
 
+    # Select a good individual from anIndividualSet
+    # Useful for a steady-state EA
     def selectGood(self, anIndividualSet):
         return self.__select__(anIndividualSet, True);
 
+    # Select a bad individual from anIndividualSet
+    # Useful for a steady-state EA
     def selectBad(self, anIndividualSet):
         return self.__select__(anIndividualSet, False);
 
+    # Run this method once per generation, before any selection is done. Useful for ranking the individuals
     def preProcess(self, anIndividualSet):
         raise NotImplementedError("Subclasses should implement this!")
 
+    # Abstract method to perform the actual selection
     def __select__(self, anIndividualSet, aFlag): # aFlag == True for selecting good individuals,
                                                   # aFlag == False for selecting bad individuals,
         raise NotImplementedError("Subclasses should implement this!")
 
-    def __str__(self):
+    # Method used for print()
+    def __str__(self) -> str:
         return "name:\t\"" + self.name + "\"";
