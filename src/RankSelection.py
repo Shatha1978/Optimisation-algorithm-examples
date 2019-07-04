00c77902ce07505d8033332d710335454217dbd9
Your Name <you@example.com>
2019-07-04 13:37:28 +0100

    Commented the class.

--- src/RankSelection.py
+++ src/RankSelection.py
@@ -1,10 +1,17 @@
+# Import the random package to select random individuals
 import random
+
+# Import the numpy package to rank individuals
 import numpy as np
 
+# Import the superclass (also called base class), which is an abstract class,
+# to implement the subclass RankSelection
 from SelectionOperator import *
 
+# The subclass that inherits of SelectionOperator
 class RankSelection(SelectionOperator):
 
+    # Constructor
     def __init__(self):
         super().__init__("Rank selection");
         self.rank_set = [];
@@ -13,6 +20,8 @@ class RankSelection(SelectionOperator):
         # Get a SystemRandom instance out of random package
         self.system_random = random.SystemRandom();
 
+    # Rank the individuals (run once per generation before any selection is done)
+    # anIndividualSet: The set of individual to choose from
     def preProcess(self, anIndividualSet):
         self.rank_set = [];
 
@@ -29,8 +38,11 @@ class RankSelection(SelectionOperator):
         for rank in self.rank_set:
             self.sum_rank += rank;
 
-    def __select__(self, anIndividualSet, aFlag): # aFlag == True for selecting good individuals,
-                                                  # aFlag == False for selecting bad individuals,
+    # Select an idividual
+    # anIndividualSet: The set of individual to choose from
+    # aFlag == True for selecting good individuals,
+    # aFlag == False for selecting bad individuals,
+    def __select__(self, anIndividualSet, aFlag):
 
         if aFlag == False:
             raise NotImplementedError("Selecting a bad individual is not implemented in RankSelection!")
