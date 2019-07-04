dd12aa4b2d5fe24812713aa70497ddb2e68c5cfd
Your Name <you@example.com>
2019-07-04 14:44:59 +0100

    Normalise the fitness values between 0 and 1 in case some are negative.

--- src/RouletteWheel.py
+++ src/RouletteWheel.py
@@ -12,6 +12,8 @@ class RouletteWheel(SelectionOperator):
     def __init__(self):
         super().__init__("Roulette wheel selection");
         self.sum_fitness = 0.0
+        self.min_fitness =  float('inf')
+        self.max_fitness = -float('inf')
 
         # Get a SystemRandom instance out of random package
         self.system_random = random.SystemRandom();
@@ -21,8 +23,17 @@ class RouletteWheel(SelectionOperator):
     def preProcess(self, anIndividualSet):
         # Compute fitness sumation
         self.sum_fitness = 0.0
+        self.min_fitness =  float('inf')
+        self.max_fitness = -float('inf')
+
         for individual in anIndividualSet:
-            self.sum_fitness += individual.fitness
+            self.min_fitness = min(self.min_fitness, individual.fitness);
+            self.max_fitness = max(self.max_fitness, individual.fitness);
+
+        range = self.max_fitness - self.min_fitness;
+
+        for individual in anIndividualSet:
+            self.sum_fitness += (individual.fitness - self.min_fitness) / range;
 
     # Select an idividual
     # anIndividualSet: The set of individual to choose from
@@ -38,7 +49,8 @@ class RouletteWheel(SelectionOperator):
 
         # Select the individual depending on the probability
         accumulator = 0.0;
+        range = self.max_fitness - self.min_fitness;
         for individual in anIndividualSet:
-            accumulator += individual.fitness;
+            accumulator += (individual.fitness - self.min_fitness) / range;
             if accumulator >= random_number:
                 return anIndividualSet.index(individual)
