a4291c49bc2a537bda8e525625b5601810aaf276
Your Name <you@example.com>
2019-09-09 17:14:22 +0100

    Save the animation of the optimisation process done using Scipy.

--- src/TestAckley.py
+++ src/TestAckley.py
@@ -5,7 +5,7 @@ from AckleyFunction import *
 import Comparison
 
 # Stopping criteria
-max_iterations = 200;
+max_iterations = 100;
 
 # Instantiate the objective function
 test_problem = AckleyFunction(2);
@@ -13,4 +13,4 @@ test_problem = AckleyFunction(2);
 # Number of runs
 number_of_runs = 15;
 
-Comparison.run(test_problem, max_iterations, number_of_runs, "ackley_", True);
+Comparison.run(test_problem, max_iterations, number_of_runs, "ackley_", tol=-1, visualisation = True, aPreCallback = None, aPostCallback = None);
