4718b78b8ae4bcdfe2bf1a3199daecb7c4e1a237
Your Name <you@example.com>
2020-02-18 12:34:19 +0000

    Add a circular list to record if the max number of flies has been reached in an iteration. It is used in the stopping criteria of the Fly algorithm. Count the number of good flies and bad flies assessed during the selection.

--- src/ThresholdSelection.py
+++ src/ThresholdSelection.py
@@ -1,9 +1,15 @@
 # Import the random package to radomly select individuals
 import random
 
+# Import the numpy package for the circular list
+import numpy as np
+
 # Import the superclass (also called base class), which is an abstract class,
 # to implement the subclass ThresholdSelection
 from SelectionOperator import *
+
+# Import the circular list
+from CircularList import *
 
 # The subclass that inherits of SelectionOperator
 class ThresholdSelection(SelectionOperator):
@@ -25,8 +31,13 @@ class ThresholdSelection(SelectionOperator):
         self.alternative_selection_operator = anAlternativeSelectionOperator;
         self.max_iteration = aMaxIteration;
         self.max_iteration_reached_counter = 0;
+        self.circular_list = CircularList(50, -1);
+
         # Get a SystemRandom instance out of random package
         self.system_random = random.SystemRandom();
+
+        self.number_of_good_flies = 0
+        self.number_of_bad_flies = 0
 
     # Nothing to do
     def preProcess(self, anIndividualSet):
@@ -54,16 +65,27 @@ class ThresholdSelection(SelectionOperator):
             if aFlag == True:
                 # The fitness is greater than the threshold, it's a good individual
                 if fitness > self.threshold:
-                    self.max_iteration_reached_counter = 0;
+                    self.number_of_good_flies += 1;
                     return selected_index;
+                else:
+                    self.number_of_bad_flies += 1;
             # Try to find a bad individual (candidate for death)
             else:
                 # The fitness is lower than or equal to the threshold, it's a bad individual
                 if fitness <= self.threshold:
-                    self.max_iteration_reached_counter = 0;
+                    self.max_iteration_reached_counter -= 1;
+                    self.circular_list.append(-1);
+                    self.number_of_bad_flies += 1;
                     return selected_index;
+                else:
+                    self.number_of_good_flies += 1;
 
         # The threshold selection has failed self.max_iteration times,
         # use self.alternative_selection_operator instead
-        self.max_iteration_reached_counter += 1;
+
+        # Try to find a bad individual (candidate for death)
+        if aFlag == False:
+            self.max_iteration_reached_counter += 1;
+            self.circular_list.append(1);
+
         return self.alternative_selection_operator.__select__(anIndividualSet, aFlag);
