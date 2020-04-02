3b7dab371b18fc4eafb52c76ad6a7e57a27b1c37
effepivi <franck.p.vidal@gmail.com>
2020-04-02 20:39:53 +0100

    Setup the environment on SCW.

---
+++ tutorials/LampProblem/env.sh
@@ -0,0 +1,10 @@
+module purge
+module load python/3.7.0
+
+PYTHONPATH=/home/b.eese10/Fly4lamps/src
+PYTHONPATH=/home/b.eese10/Fly4lamps/examples:$PYTHONPATH
+PYTHONPATH=/home/b.eese10/Fly4lamps/tutorials:$PYTHONPATH
+
+
+export PYTHONPATH
+
